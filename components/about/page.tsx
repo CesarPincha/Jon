4f36385b6fde8b240abe793ed7a3d3715309bc91
Cesar <cesars@Cesars-MacBook-Pro.local>
2024-07-16 10:14:59 -0500

    fixed about section

--- components/about/page.tsx
+++ components/about/page.tsx
@@ -13,28 +13,36 @@ const About = () => {
           Rooftop Solutions
         </h1>
         <p className="p-4  text-start">
-          At Rooftop Solutions, our primary goal is to provide top-tier roofing
-          services that exceed customer expectations. We strive to deliver
-          quality craftsmanship and exceptional service in every project we
-          undertake, ensuring durability and satisfaction. Our dedication to
-          excellence drives us to continually innovate and adopt the latest
-          industry practices, making us a leader in the roofing sector.
+          An old Irish Proverb reads “May the roof above us never fall in. And
+          may us friends gathered below it never fall out.” We at RoofTop
+          Solutions believe we make sure your roof never falls in, and we are
+          positive that your experience with us will make us life long friends.
+          I’m Brian Olagues, a lifelong Louisiana resident, born and raised in
+          Lockport, graduated from Jesuit High School, and played college
+          baseball at Loyola University in New Orleans.
         </p>
         <p className="p-4 text-start ">
-          Our company culture is built on the pillars of integrity, teamwork,
-          and continuous improvement. We believe in fostering a collaborative
-          environment where every team member is valued and empowered to
-          contribute their best. This collective effort ensures that we not only
-          meet but exceed the standards of quality and safety in our work,
-          creating roofs that stand the test of time.
+          With over 18 years of experience in the roofing industry that
+          followed, I know the challenges Louisiana homeowners face. No, it’s
+          not weather events or hurricanes. Those things we cannot control, but
+          we know are possible. The biggest issue facing Louisiana homeowners is
+          who are they going to select to be my roofer and are they qualified to
+          do the job that should be the standard? It’s not an easy question to
+          answer because there are so many “roofers“ in Louisiana because it is
+          so easy to become one, per state laws and licensures. Throw in some
+          fancy websites, smooth talking sales rep, and a marketing campaign,
+          and you have your overnight “roofer.” That’s not us. In fact those
+          companies only intensify how hard it is for a homeowner to understand
+          who really has their best interest in mind and who’s trying to make a
+          quick buck.
         </p>
         <p className="p-4 text-start ">
-          Rooftop Solutions is committed to sustainability and community
-          involvement. We use eco-friendly materials and practices to minimize
-          our environmental footprint, and we actively participate in local
-          initiatives to give back to the community. Our mission is to build
-          strong, lasting relationships with our clients and partners, grounded
-          in trust and mutual respect.
+          At RoofTop Solutions, we combine experience with integrity. If we
+          wouldn’t do it on our own house, we wouldn’t do it on yours. We have
+          to sleep at night, and sleeping at night is knowing that we did the
+          right thing for our homeowners, we didn’t half ass anything or cut any
+          corners, and that we gave you our best because only the best is good
+          enough.
         </p>
       </div>
       <div className="flex-1 flex justify-center items-center text-center p-4 rounded-md">
